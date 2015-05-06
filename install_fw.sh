68d6e840702278966e6f2055a6ca945023ecce09
Hans Kirchner <hans-kirchner@gmx.de>
2015-05-06 11:55:50 +0200

    Fix problems with OGRE OIS

--- install_fw.sh
+++ install_fw.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #install ogre
 #For debian
-#    add OGRE PPA with: 
+#    add OGRE PPA with:
 sudo add-apt-repository ppa:ogre-team/ogre
 #    update your package list with:
 sudo apt-get update
-#    install libogre-dev package with: 
-sudo apt-get install libogre-1.9-dev
+#    install libogre-dev package with:
+sudo apt-get install libogre-1.9-dev libois-1.3.0 libois-dev
 echo "installed ogre"
 
 #For macosx
